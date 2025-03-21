2dcd507d799cfe8234926d59cb3f437a8e017c24
Aarón Roque Fonseca <aaronroquefonseca@gmail.com>
2025-03-21 14:40:20 +0100

    guestId will not be needed for now with the new arquitecture

--- frontend/src/types/userTypes.ts
+++ frontend/src/types/userTypes.ts
@@ -5,6 +5,6 @@ export type User = {
     plexPinCode?: string;   // cookie, expires in 15 minutes
     plexToken?: string;     // storage, no expiration, plex will revoke over time
     jellyfinKey?: string;
-    guestId?: string;       // session exclusive, don't store
+    // guestId?: string;       // session exclusive, don't store
     sessionId?: string;     // session exclusive, don't store
 }
