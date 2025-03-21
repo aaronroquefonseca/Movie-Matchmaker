b9534e7412a140668be87360be34ebfa6140d584
Aarón Roque Fonseca <aaronroquefonseca@gmail.com>
2025-03-21 14:51:16 +0100

    feat: implement clientId generation and storage in localStorage

--- frontend/src/app.tsx
+++ frontend/src/app.tsx
@@ -1,8 +1,11 @@
 import './app.css'
 import {Navbar} from "./components/Navbar.tsx";
 import {createContext} from "preact";
-import {useState} from "preact/hooks"
+import {useState, useEffect} from "preact/hooks"
 import {User} from "./types/userTypes.ts";
+import * as plex from "./scripts/plex.ts";
+
+import { v4 as uuidv4 } from "uuid";
 
 const UserContext = createContext<any>(null);
 
@@ -10,6 +13,22 @@ export const App = () => {
     const [user, setUser] = useState<User>({
         clientId: ""
     });
+
+    useEffect(() => {
+        let storedClientId = localStorage.getItem("clientId");
+        
+        // If no clientId exists, generate one and store it
+        if (!storedClientId) {
+            storedClientId = uuidv4();  // Generate a new UUID
+            localStorage.setItem("clientId", storedClientId);  // Save it to localStorage
+        }
+        
+        // Update the state with the clientId
+        setUser(prevState => ({
+            ...prevState,
+            clientId: storedClientId
+        }));
+    }, []);
 
     return <UserContext.Provider value={{user, setUser}}>
         <Navbar />
