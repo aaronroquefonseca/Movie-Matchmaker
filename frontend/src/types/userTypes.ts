92ffd9be04af535c164c3de6ffeb4ce7e398c56e
Aarón Roque Fonseca <aaronroquefonseca@gmail.com>
2025-03-21 02:42:02 +0100

    feat: update user types to include plexPinId, plexPinCode, and plexToken for enhanced authentication

--- frontend/src/types/userTypes.ts
+++ frontend/src/types/userTypes.ts
@@ -1,8 +1,10 @@
 
 export type User = {
-    clientId: string;
-    plexKey?: string;
+    clientId: string;       // storage, no expiration
+    plexPinId?: string;     // cookie, expires in 15 minutes
+    plexPinCode?: string;   // cookie, expires in 15 minutes
+    plexToken?: string;     // storage, no expiration, plex will revoke over time
     jellyfinKey?: string;
-    guestId?: string;
-    sessionId?: string;
+    guestId?: string;       // session exclusive, don't store
+    sessionId?: string;     // session exclusive, don't store
 }
